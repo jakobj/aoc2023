f6ed3da214956dc0f483055bd019d4e997d983f9
Jakob Jordan <jakobjordan@posteo.de>
2023-12-16 07:19:22 -0500

    Solve day 6 part 2

--- src/bin/main-6.rs
+++ src/bin/main-6.rs
@@ -6,20 +6,34 @@ fn main() {
         fs::read_to_string(filename).expect("Should have been able to read the input file");
 
     let content = content.lines().collect::<Vec<&str>>();
+
+    // part 1
     let time = extract_numbers_after_colon(content[0]);
     let record_distance = extract_numbers_after_colon(content[1]);
     let possible_distances = time
-        .iter()
+        .into_iter()
         .map(compute_distances)
         .collect::<Vec<Vec<usize>>>();
     let nways = possible_distances
-        .iter()
-        .zip(record_distance.iter())
-        .map(|(pd, &rd)| pd.iter().filter(|&&d| d > rd).count())
+        .into_iter()
+        .zip(record_distance.into_iter())
+        .map(|(pd, rd)| pd.iter().filter(|&&d| d > rd).count())
         .collect::<Vec<usize>>();
     println!(
         "When you multiply the number of ways you can beat the record together you get {:?}.",
         nways.iter().product::<usize>()
+    );
+
+    // brute force part 2
+    let time = extract_single_number_after_colon(content[0]);
+    let record_distance = extract_single_number_after_colon(content[1]);
+    let possible_distances = compute_distances(time);
+    let nways = possible_distances
+        .into_iter()
+        .filter(|&pd| pd > record_distance)
+        .count();
+    println!(
+        "There are {nways} ways in which you can beat the record in this one much longer race."
     );
 }
 
@@ -31,9 +45,17 @@ fn extract_numbers_after_colon(s: &str) -> Vec<usize> {
         .collect::<Vec<usize>>()
 }
 
-fn compute_distances(t: &usize) -> Vec<usize> {
-    (0..*t + 1)
+fn compute_distances(t: usize) -> Vec<usize> {
+    (0..t + 1)
         .into_iter()
-        .map(|ti| (*t - ti) * ti)
+        .map(|ti| (t - ti) * ti)
         .collect::<Vec<usize>>()
+}
+
+fn extract_single_number_after_colon(s: &str) -> usize {
+    s.split(':').collect::<Vec<&str>>()[1]
+        .split(' ')
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap()
 }
