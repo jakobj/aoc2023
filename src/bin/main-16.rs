1d9c8b1e66291a9d6161081066d5037eba6adc79
Jakob Jordan <jakobjordan@posteo.de>
2023-12-30 15:55:54 -0500

    Solve day 16 part 2

--- src/bin/main-16.rs
+++ src/bin/main-16.rs
@@ -10,82 +10,29 @@ fn main() {
         .map(|l| l.chars().map(Tile::from).collect::<Vec<Tile>>())
         .collect::<Vec<Vec<Tile>>>();
 
-    let mut stack = Vec::new();
-    stack.push(State {
-        position: Position { y: 0, x: 0 },
-        direction: Direction::Right,
-    });
-    let mut visited = HashSet::new();
-    while let Some(state) = stack.pop() {
-        if visited.contains(&state) {
-            continue;
-        }
-        visited.insert(state);
-
-        let tile = grid[state.position.y as usize][state.position.x as usize];
-        let mut potential_states = Vec::new();
-        match state.direction {
-            Direction::Up => {
-                if tile == Tile::Empty || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_up(state));
-                }
-                if tile == Tile::SlashMirror || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_right(state));
-                }
-                if tile == Tile::BackSlashMirror || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_left(state));
-                }
-            }
-            Direction::Down => {
-                if tile == Tile::Empty || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_down(state));
-                }
-                if tile == Tile::SlashMirror || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_left(state));
-                }
-                if tile == Tile::BackSlashMirror || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_right(state));
-                }
-            }
-            Direction::Left => {
-                if tile == Tile::Empty || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_left(state));
-                }
-                if tile == Tile::SlashMirror || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_down(state));
-                }
-                if tile == Tile::BackSlashMirror || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_up(state));
-                }
-            }
-            Direction::Right => {
-                if tile == Tile::Empty || tile == Tile::HorizontalSplitter {
-                    potential_states.push(move_right(state));
-                }
-                if tile == Tile::SlashMirror || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_up(state));
-                }
-                if tile == Tile::BackSlashMirror || tile == Tile::VerticalSplitter {
-                    potential_states.push(move_down(state));
-                }
-            }
-        }
-        for state in potential_states.into_iter() {
-            if state.position.y >= 0
-                && state.position.y < grid.len() as i32
-                && state.position.x >= 0
-                && state.position.x < grid[0].len() as i32
-            {
-                stack.push(state);
-            }
-        }
-    }
-    let energized_tiles = visited
-        .into_iter()
-        .map(|s| s.position)
-        .collect::<HashSet<Position>>()
-        .len();
+    let energized_tiles = count_energized_tiles(&grid, Position { y: 0, x: 0 }, Direction::Right);
     println!("{energized_tiles} tiles end up being energized.");
+
+    let binding = [
+        (0..grid[0].len())
+            .map(|x| count_energized_tiles(&grid, Position { y: 0, x: x as i32 }, Direction::Down))
+            .max()
+            .unwrap(),
+        (0..grid[0].len())
+            .map(|x| count_energized_tiles(&grid, Position {y: grid.len() as i32 - 1, x: x as i32,}, Direction::Up))
+            .max()
+            .unwrap(),
+        (0..grid[0].len())
+            .map(|y| count_energized_tiles(&grid, Position { y: y as i32, x: 0 }, Direction::Right))
+            .max()
+            .unwrap(),
+        (0..grid[0].len())
+            .map(|y| count_energized_tiles(&grid, Position {y: y as i32, x: grid[0].len() as i32 - 1,}, Direction::Left))
+            .max()
+            .unwrap(),
+    ];
+    let most_energized_tiles = binding.iter().max().unwrap();
+    println!("The largest number of energized tiles is {most_energized_tiles}.")
 }
 
 fn move_up(state: State) -> State {
@@ -168,4 +115,87 @@ enum Direction {
     Down,
     Left,
     Right,
+}
+
+fn count_energized_tiles(
+    grid: &[Vec<Tile>],
+    starting_position: Position,
+    starting_direction: Direction,
+) -> usize {
+    let mut stack = Vec::new();
+    stack.push(State {
+        position: starting_position,
+        direction: starting_direction,
+    });
+    let mut visited = HashSet::new();
+    while let Some(state) = stack.pop() {
+        if visited.contains(&state) {
+            continue;
+        }
+        visited.insert(state);
+
+        let tile = grid[state.position.y as usize][state.position.x as usize];
+        let mut potential_states = Vec::new();
+        match state.direction {
+            Direction::Up => {
+                if tile == Tile::Empty || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_up(state));
+                }
+                if tile == Tile::SlashMirror || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_right(state));
+                }
+                if tile == Tile::BackSlashMirror || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_left(state));
+                }
+            }
+            Direction::Down => {
+                if tile == Tile::Empty || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_down(state));
+                }
+                if tile == Tile::SlashMirror || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_left(state));
+                }
+                if tile == Tile::BackSlashMirror || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_right(state));
+                }
+            }
+            Direction::Left => {
+                if tile == Tile::Empty || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_left(state));
+                }
+                if tile == Tile::SlashMirror || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_down(state));
+                }
+                if tile == Tile::BackSlashMirror || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_up(state));
+                }
+            }
+            Direction::Right => {
+                if tile == Tile::Empty || tile == Tile::HorizontalSplitter {
+                    potential_states.push(move_right(state));
+                }
+                if tile == Tile::SlashMirror || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_up(state));
+                }
+                if tile == Tile::BackSlashMirror || tile == Tile::VerticalSplitter {
+                    potential_states.push(move_down(state));
+                }
+            }
+        }
+        for state in potential_states.into_iter() {
+            if state.position.y >= 0
+                && state.position.y < grid.len() as i32
+                && state.position.x >= 0
+                && state.position.x < grid[0].len() as i32
+            {
+                stack.push(state);
+            }
+        }
+    }
+
+    visited
+        .into_iter()
+        .map(|s| s.position)
+        .collect::<HashSet<Position>>()
+        .len()
 }
