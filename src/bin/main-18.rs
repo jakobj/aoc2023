f5e34754aeb63aaa7554b50af7b5682154f9efdf
Jakob Jordan <jakobjordan@posteo.de>
2024-01-02 12:57:48 -0500

    Solve day 18 part 2

--- src/bin/main-18.rs
+++ src/bin/main-18.rs
@@ -9,19 +9,35 @@ fn main() {
         .lines()
         .map(|l| {
             let l = l.split(' ').collect::<Vec<&str>>();
-            Instruction {
-                direction: Direction::from(l[0]),
-                distance: l[1].parse::<i32>().unwrap(),
-                color: Color::from(&l[2][2..l[2].len() - 1]),
-            }
+            (
+                Instruction {
+                    direction: Direction::from(l[0]),
+                    distance: l[1].parse::<i32>().unwrap(),
+                },
+                Color::from(&l[2][2..l[2].len() - 1]),
+            )
         })
-        .collect::<Vec<Instruction>>();
+        .collect::<Vec<(Instruction, Color)>>();
 
     // dig loop
-    let mut ring = Vec::new();
+    let perimeter = dig_perimeter(&instructions, false);
+    let volume = compute_volume(&perimeter);
+    println!("The lagoon could hold {volume} cubic meters of lava.");
+
+    // dig loop with decoded colors
+    let perimeter = dig_perimeter(&instructions, true);
+    let volume = compute_volume(&perimeter);
+    println!("The lagoon could hold {volume} cubic meters of lava.");
+}
+
+fn dig_perimeter(instructions: &[(Instruction, Color)], use_color: bool) -> Vec<Position> {
+    let mut perimeter = Vec::new();
     let mut position = Position { y: 0, x: 0 };
-    ring.push(position);
-    for ins in instructions.iter() {
+    perimeter.push(position);
+    for (mut ins, color) in instructions.iter() {
+        if use_color {
+            ins = color.decode();
+        }
         for _ in 0..ins.distance {
             (position.y, position.x) = match ins.direction {
                 Direction::Up => (position.y - 1, position.x),
@@ -29,34 +45,35 @@ fn main() {
                 Direction::Left => (position.y, position.x - 1),
                 Direction::Right => (position.y, position.x + 1),
             };
-            ring.push(position);
+            perimeter.push(position);
         }
     }
     assert!(position.y == 0 && position.x == 0);
-    ring.pop();
+    perimeter.pop();
+    perimeter
+}
 
+fn compute_volume(perimeter: &[Position]) -> i64 {
     // calculate are of polygon
     // https://stackoverflow.com/questions/451426/how-do-i-calculate-the-area-of-a-2d-polygon
     // https://web.archive.org/web/20100405070507/http://valis.cs.uiuc.edu/~sariel/research/CG/compgeom/msg00831.html
-    let mut volume = 0;
-    for i in 0..ring.len() {
-        let j = (i + 1) % ring.len();
-        volume += (ring[i].x + 1) * (ring[j].y + 1);
-        volume -= (ring[i].y + 1) * (ring[j].x + 1);
+    let mut volume: i64 = 0;
+    for i in 0..perimeter.len() {
+        let j = (i + 1) % perimeter.len();
+        volume += (perimeter[i].x + 1) as i64 * (perimeter[j].y + 1) as i64;
+        volume -= (perimeter[i].y + 1) as i64 * (perimeter[j].x + 1) as i64;
     }
     volume /= 2;
 
     // control for ring "area"
-    volume += ring.len() as i32 / 2 + 1;
-
-    println!("The lagoon could hold {volume} cubic meters of lava.")
+    volume += perimeter.len() as i64 / 2 + 1;
+    volume
 }
 
 #[derive(Clone, Copy, Debug)]
 struct Instruction {
     direction: Direction,
     distance: i32,
-    color: Color,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -84,6 +101,24 @@ struct Color {
     r: u8,
     g: u8,
     b: u8,
+}
+
+impl Color {
+    fn decode(&self) -> Instruction {
+        let s = format!("{:02x}{:02x}{:02x}", self.r, self.g, self.b);
+        let distance = i32::from_str_radix(&s[..s.len() - 1], 16).unwrap();
+        let direction = match &s[s.len() - 1..s.len()] {
+            "0" => Direction::Right,
+            "1" => Direction::Down,
+            "2" => Direction::Left,
+            "3" => Direction::Up,
+            _ => panic!("Unexpected direction {}", &s[s.len() - 1..s.len()]),
+        };
+        Instruction {
+            direction,
+            distance,
+        }
+    }
 }
 
 impl From<&str> for Color {
